769a207aa8290b2e68ca777447245523022a0333
Anabella Falkenberg <anabella.falk@gmail.com>
2025-08-11 17:52:50 -0400

    Create data prep functions for model fitting

--- data_prep.py
+++ data_prep.py
@@ -2,6 +2,9 @@ import kagglehub
 import numpy as np
 import pandas as pd
 
+from sklearn.cluster import KMeans
+
+### Load and Split Data
 """
 Load training data from kaggle. 
 Returns pandas df with "Unnamed: 0" column dropped.
@@ -45,3 +48,66 @@ Returns two pandas df each of training and validation data.
 """
 def load_train_split():
     return split_train_val(load_train())
+
+
+### Extract Date Info
+"""
+Modifies dataframe `dat` creating the new column 'trans_year' containing the 
+year of the transaction.
+"""
+def extract_year(dat):
+    dat['trans_year'] = dat['trans_date_trans_time'].apply(lambda x: x[:4])
+
+"""
+Modifies dataframe `dat` creating the new column 'trans_month' containing the 
+month of the transaction.
+"""
+def extract_month(dat):
+    dat['trans_month'] = dat['trans_date_trans_time'].apply(lambda x: int(x[5:7]))
+
+"""
+Modifies dataframe `dat` creating the new column 'trans_month_year' containing the 
+month and year of the transaction.
+"""
+def extract_month_year(dat):
+    dat['trans_month_year'] = dat['trans_date_trans_time'].apply(lambda x: x[:7])
+
+
+### Extract Age
+"""
+Modifies dataframe `dat` creating the new column 'age_at_trans' containing the 
+age of credit card owner at time of transaction.
+"""
+def extract_age(dat):
+    trans_date = pd.to_datetime(dat['trans_date_trans_time'].apply(lambda x: x[:10]))
+    dob = pd.to_datetime(dat['dob'])
+    dat['age_at_trans'] = (trans_date - dob).dt.days // 365
+
+
+### Bin Jobs
+"""
+FIt KMeans with 3 clusters to the 'job' column to get bins. 
+Returns mapping for bins
+"""
+def fit_job_bins(dat):
+    cluster = KMeans(3)
+    fraud_job = dat.groupby('job')['is_fraud'].mean()
+    job_bin = pd.Series(cluster.fit_predict(fraud_job.values.reshape(-1,1)))
+    job_bin.index = fraud_job.index
+    return job_bin
+
+"""
+Modifies dataframe `dat` adding the binned column 'job_bin' for the column 'job'
+using `bin_map` as binning mapping
+"""
+def bin_jobs(dat, bin_map):
+    dat['job_bin'] = dat['job'].apply(lambda x: bin_map.loc[x])
+
+
+### Extract Distance
+"""
+Modifies dataframe `dat` creating the new column 'distance' containing the manhattan
+distance from the owner's address to the merchant.
+"""
+def extract_distance(dat):
+    dat['distance'] = np.sqrt((dat['merch_lat'] - dat['lat'])**2 + (dat['merch_long'] - dat['long'])**2)
